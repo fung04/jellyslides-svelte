32fc89a36c60d171bd65863030a20f495a2e7685
fung04 <chifungchan420@gmail.com>
2026-01-27 22:49:43 +0800

    added path /jellyslides-svelte

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,12 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			fallback: '404.html' // Use 404.html for SPA fallback if needed
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/jellyslides-svelte' : ''
+		}
 	}
 };
 
